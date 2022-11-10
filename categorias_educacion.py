fffffd4d58fb64e8cd1f0455489196440c1f3ae6
CristobalPerez-Cotapos <cprezcotapos@uc.cl>
2022-11-09 22:59:45 -0300

    arreglos nv

--- categorias_educacion.py
+++ categorias_educacion.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
-datos = pd.read_csv("Modelo jefes de hogar/casen_2011.csv")
+datos = pd.read_csv("Modelo jefes de hogar/casen_2017.csv")
 df = pd.DataFrame(datos)
 
 df = df.replace({'e6a': {
-    1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1,
-    7: 2, 8: 2, 9: 2, 10: 2,
-    11: 3, 12: 3,
-    13: 4
+    1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1,
+    8: 2, 9: 2, 10: 2, 11: 2,
+    12: 3, 13: 3, 14: 3, 15: 3,
+    16: 4, 17: 4
+}})
+
+df = df.replace({'nv': {
+    99: 0
 }})
 
 # 1 sería menos que media
@@ -15,4 +19,4 @@ df = df.replace({'e6a': {
 # 3 sería universitaria
 # 4 sería postgrado
 
-df.to_csv("Modelo jefes de hogar/casen_2011.csv", index=False)
+df.to_csv("Modelo jefes de hogar/casen_2017.csv", index=False)
--- relacionador_hijos.py
+++ relacionador_hijos.py
@@ -29,6 +29,7 @@ for index_1, row_1 in df.iterrows():
     diccionario[row_1["folio"]].append([row_1, index_1])
 
 print("diccionario cargado")
+print(df.head(5))
 
 for i in diccionario.keys():     #Hay que ajustarlo para los cuestionarios de distintos años
     for j in diccionario[i]:     # Por ejemplo en 2011 y 2017 los numero indican cosas distintas
@@ -42,4 +43,5 @@ for i in diccionario.keys():     #Hay que ajustarlo para los cuestionarios de di
             elif (j[0]["pco2"] == 2) and k[0]["pco2"] == 6:
                 if j[0]["nv"] > j[0]["edad"] - k[0]["edad"]:
                     df.at[j[1], "nv"] = j[0]["edad"] - k[0]["edad"]
+print(df.head(5))
 df.to_csv("Modelo jefes de hogar/casen_2015.csv", index=False)
