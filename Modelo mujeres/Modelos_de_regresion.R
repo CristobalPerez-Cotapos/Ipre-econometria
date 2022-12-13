3dd64f8ef63cc6d481c1cbbdd2d7d951a53df61f
trinidad aguilera <trini.aguilera2@uc.cl>
2022-12-12 23:02:36 -0300

    final

--- Modelo mujeres/Modelos_de_regresion.R
+++ Modelo mujeres/Modelos_de_regresion.R
@@ -1,6 +1,8 @@
 library(readxl)
 #BDD <- read_excel("C:/Users/cpere/Desktop/Libros Ingeniería/Sexto semestre/Ipre-econometria/Modelo mujeres/m13_m15.xlsx")
-BDD <- read_excel("C:/Users/cpere/Desktop/Libros Ingeniería/Sexto semestre/Ipre-econometria/Modelo mujeres/m15_m17.xlsx")
+#BDD <- read_excel("C:/Users/cpere/Desktop/Libros Ingeniería/Sexto semestre/Ipre-econometria/Modelo mujeres/m15_m17.xlsx")
+BDD <- read_excel("C:/Users/trini/OneDrive/Escritorio/GITHUB COSAS/Ipre-econometria/Modelo mujeres/m11_m13.xlsx")
+View(BDD)
 
 numero_en_mach = vector()
 id = 0
@@ -34,6 +36,9 @@ vector_hijos[1:50]
 
 modelo = lm(BDD$ytrabaj ~ BDD$nhijos)
 summary(modelo)
+
+modelo_log = lm(log(BDD$ytrabaj) ~ BDD$nhijos)
+summary(modelo_log)
 
 modelo_2 = lm(BDD$ytrabaj ~ vector_hijos)
 summary(modelo_2)
